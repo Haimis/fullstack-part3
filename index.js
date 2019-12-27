dc22f20e0dc9b9e61368a48ddcfb672b9d3f43ae
Haimis <haimila.mikko@gmail.com>
2019-12-27 23:25:22 +0200

    paluu projektiin

--- index.js
+++ index.js
@@ -1,72 +1,53 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+//const morgan = require('morgan')
 const cors = require('cors')
-
-
-
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
+
+
+
 app.use(cors())
 app.use(express.static('build'))
-app.use(morgan(':method :url :status :res[content-length] :response-time ms :content'))
-
-
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-
-    {
-        name: "Dan Abramov",
-        number: "12-43-234335",
-        id: 3
-    },
-
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
-]
+//app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 app.get('/api/persons', (request, response) => {
-    response.send(persons)
-    morgan.token('content', () => {return JSON.stringify(request.body)})
+    Person.find({}).then(result => {
+        response.json(result.map(person => person.toJSON()))
+      })
+//      morgan.token('content', () => {return JSON.stringify(request.body)})    
 })
 
 app.get('/info', (request, response) => {
-    const contacts = persons.length
+    const contacts = people.length
     const date = Date()
-    morgan.token('content', () => {return JSON.stringify(request.body)})
+//    morgan.token('content', () => {return JSON.stringify(request.body)})
     response.send(`Phonebook has ${contacts} contacts<br>
     ${date}`)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    morgan.token('content', () => {return JSON.stringify(person)})
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person.toJSON())
+        } else {
+            response.status(204).end()
+        }
+        
+    })
+    .catch(error => next(error))
+    
+    
+//    morgan.token('content', () => {return JSON.stringify(person)})
+    
 })
 
 app.post('/api/persons', (request, response) => {
-    const randomId = Math.floor(Math.random() * Math.floor(1000000))
     const body = request.body
-    morgan.token('content', () => {return (JSON.stringify(body))})
+//    morgan.token('content', () => {return (JSON.stringify(body))})
    if (!body.name) {
         return response.status(400).json({
             error: 'name missing'
@@ -75,32 +56,61 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'number missing'
         })
-    } else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
     } else {
-
-
-        const person = {
+        const person = new Person({
             name: body.name,
-            number: body.number,
-            id: randomId
-        }
-        persons = persons.concat(person)
-
-        response.json(person)
+            number: body.number
+        })
+        person.save().then(savedPerson => {
+            response.json(savedPerson.json)
+        })
     }
+
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+        response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+//    morgan.token('content', () => {return (JSON.stringify(body))})
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+console.error(error.message)
+
+if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+}
+
+next(error)
+}
+
+
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
